1d0b9a9a277cb4890b5a0cf60c3f1bc90e18f426
Valeria Vargas <kvargas10550@ufide.ac.cr>
2025-03-13 22:16:58 -0600

    Creación de Opciones de Menú, con cada una de las historias de usuario

---
+++ src/main/java/com/proyecto_D/PageController.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto_D;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author valer
+ */
+@Controller
+public class PageController {
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/listaMascotas")
+    public String listaMascotasPage() {
+        return "listaMascotas"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/necesidades")
+    public String necesidadesPage() {
+        return "necesidades"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/registro")
+    public String registroPage() {
+        return "registro"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/inscribirse")
+    public String inscribirsePage() {
+        return "inscribirse"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/testimonios")
+    public String testimoniosPage() {
+        return "testimonios"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/requisitosAdopcion")
+    public String requisitosAdopcionPage() {
+        return "requisitosAdopcion"; // This should match about.html in src/main/resources/templates
+    }
+    
+    @GetMapping("/castraciones")
+    public String castracionesPage() {
+        return "castraciones"; // This should match about.html in src/main/resources/templates
+    }
+}
